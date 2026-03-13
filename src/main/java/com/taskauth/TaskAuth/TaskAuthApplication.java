eb951d9a248c5c681edf3b3f6c8ef8f023035799
John Christian Vicente <johnc.vicente1@gmail.com>
2026-03-13 21:00:57 +0800

    Added keys

--- src/main/java/com/taskauth/TaskAuth/TaskAuthApplication.java
+++ src/main/java/com/taskauth/TaskAuth/TaskAuthApplication.java
@@ -2,8 +2,11 @@ package com.taskauth.TaskAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:env.properties")
+
 public class TaskAuthApplication {
 
 	public static void main(String[] args) {
